6dbb3be53052a66da0d6e8741867abf5a877a51f
Martin1984K <martin.kloos@gmx.net>
2024-05-10 14:00:32 +0200

    Created definition class that gets a word as input and return its definition

--- definition.py
+++ definition.py
@@ -1,7 +1,16 @@
+import pandas as pd
+
+
 class Definition:
 
     def __init__(self, term):
         self.term = term
 
     def get(self):
-        pass
+        df = pd.read_csv('data.csv')
+        return tuple(df.loc[df['word']==self.term]['definition'])
+
+
+if __name__ == '__main__':
+    d = Definition('graphite')
+    print(d.get())
