48781bac90179f867bab305d96894ed1941d8543
Martin1984K <martin.kloos@gmx.net>
2024-05-11 11:34:36 +0200

    Created the main file and modified about and home

--- webapp/home.py
+++ webapp/home.py
@@ -1,16 +1,15 @@
 import justpy as jp
 
-class Home:
 
-    path = '/home'
+class Home:
+    path = '/'
+
     def serve(self):
         wp = jp.QuasarPage(tailwind=True)
-        div = jp.Div(a=wp, text="We are home, finally", classes="bg-gray-200 h-screen")
+        div = jp.Div(a=wp, classes='bg-gray-200 h-screen')
+        jp.Div(a=div ,text='This is the Home page', classes= 'text-4xl m-2')
         jp.Div(a=div, text="""
-        Blabliblup
-        """, classes="text-lg")
-
+        Blablabliblupblupblup
+        """, classes='text-lg')
         return wp
 
-jp.Route(Home.path, Home.serve)
-jp.justpy()
