a89e1d9298c9f2248b5e5ad95fd39881538facc4
Martin1984K <martin.kloos@gmx.net>
2024-05-13 09:55:42 +0200

    Corrected some minor style issues

--- webapp/about.py
+++ webapp/about.py
@@ -7,7 +7,7 @@ class About:
     def serve(self):
         wp = jp.QuasarPage(tailwind=True)
         div = jp.Div(a=wp, classes='bg-gray-200 h-screen')
-        jp.Div(a=div ,text='This is the About page', classes= 'text-4xl m-2')
+        jp.Div(a=div, text='This is the About page', classes='text-4xl m-2')
         jp.Div(a=div, text="""
         Blablabliblupblupblup
         """, classes='text-lg')
